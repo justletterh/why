dfac5a14be56d0e7cd837ce8444b17ab13736ee3
justletterh <63440442+justletterh@users.noreply.github.com>
2020-09-11 22:30:11 -0500

    h

--- run.cmd
+++ run.cmd
@@ -1,17 +1,22 @@
 @echo off
-call build&&rem builds the executables for the compiled langs
+rem builds the executables for the compiled langs
+call build
 py -3 h.py
 node e.js
 lua l1.lua
 ruby l2.rb
 o&&rem o.c
 julia space.jl
-w&&rem w.go
+rem w.go
+w
 bash o2.bash
 perl r.pl
 php l3.php
-d&&rem d.rs
+rem d.rs
+d
 call !1.bat
 tclsh !2.tcl
-!3&&rem !3.bf
-del !3.exe d.exe d.pdb o.exe w.exe&&rem deletes the compiled executables
+rem !3.bf
+!3
+rem deletes the compiled executables
+del !3.exe d.exe d.pdb o.exe w.exe
