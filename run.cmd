0d112c751352fdd96e5938e44b834e43de8a8546
justletterh <63440442+justletterh@users.noreply.github.com>
2020-09-11 22:28:30 -0500

    added comments on run.cmd

--- run.cmd
+++ run.cmd
@@ -1,17 +1,17 @@
 @echo off
-call build
+call build&&rem builds the executables for the compiled langs
 py -3 h.py
 node e.js
 lua l1.lua
 ruby l2.rb
-o
+o&&rem o.c
 julia space.jl
-w
+w&&rem w.go
 bash o2.bash
 perl r.pl
 php l3.php
-d
+d&&rem d.rs
 call !1.bat
 tclsh !2.tcl
-!3
-del !3.exe d.exe d.pdb o.exe w.exe
+!3&&rem !3.bf
+del !3.exe d.exe d.pdb o.exe w.exe&&rem deletes the compiled executables
